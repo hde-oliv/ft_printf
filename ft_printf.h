14a24cc8da077e9c544e0e0a78c90654f10a5104
hde-oliv <henriquerocha@tutanota.com>
2021-07-23 10:23:25 -0300

    style: add dots at the end of lines

--- ft_printf.h
+++ ft_printf.h
@@ -6,9 +6,9 @@
 # include <stdlib.h>
 
 /**
- * ft_printf - Prints a formatted string
+ * ft_printf - Prints a formatted string.
  *
- * @format: The base string
+ * @format: The base string.
  *
  * Description: It can handle the following arguments
  * (converter flags) such as c, s, p, d, i, u, x
@@ -19,106 +19,106 @@
 int		ft_printf(const char *format, ...);
 
 /**
- * send_output - Send a string to STDOUT
+ * send_output - Send a string to STDOUT.
  *
- * @string: A string
+ * @string: A string.
  *
  * Return: The number of chars printed.
  */
 int		send_output(char *string);
 
 /**
- * tag_handler - Deals with the incidence of % in a string
+ * tag_handler - Deals with the incidence of % in a string.
  *
  * Description: Especific to ft_printf, it's a bad idea to
  * use it on other things.
  *
- * @string: Pointer to a string
- * @args: A va_list
+ * @string: Pointer to a string.
+ * @args: A va_list.
  *
  * Return: The number of chars printed.
  */
 int		tag_handler(char **string, va_list args);
 
 /**
- * char_handler - Deals with the incidence of the c converter in a string
+ * char_handler - Deals with the incidence of the c converter in a string.
  *
  * Description: Especific to ft_printf, it's a bad idea to
  * use it on other things.
  *
- * @args: A va_list
+ * @args: A va_list.
  *
  * Return: The number of chars printed.
  */
 int		char_handler(va_list args);
 
 /**
- * str_handler - Deals with the incidence of the s converter in a string
+ * str_handler - Deals with the incidence of the s converter in a string.
  *
  * Description: Especific to ft_printf, it's a bad idea to
  * use it on other things.
  *
- * @args: A va_list
+ * @args: A va_list.
  *
  * Return: The number of chars printed.
  */
 int		str_handler(va_list args);
 
 /**
- * int_handler - Deals with the incidence of the i or d converter in a string
+ * int_handler - Deals with the incidence of the i or d converter in a string.
  *
  * Description: Especific to ft_printf, it's a bad idea to
  * use it on other things.
  *
- * @args: A va_list
+ * @args: A va_list.
  *
  * Return: The number of chars printed.
  */
 int		int_handler(va_list args);
 
 /**
- * uint_handler - Deals with the incidence of the u converter in a string
+ * uint_handler - Deals with the incidence of the u converter in a string.
  *
  * Description: Especific to ft_printf, it's a bad idea to
  * use it on other things.
  *
- * @args: A va_list
+ * @args: A va_list.
  *
  * Return: The number of chars printed.
  */
 int		uint_handler(va_list args);
 
 /**
- * ptr_handler - Deals with the incidence of the p converter in a string
+ * ptr_handler - Deals with the incidence of the p converter in a string.
  *
  * Description: Especific to ft_printf, it's a bad idea to
  * use it on other things.
  *
- * @args: A va_list
+ * @args: A va_list.
  *
  * Return: The number of chars printed.
  */
 int		ptr_handler(va_list args);
 
 /**
- * hex_handler - Deals with the incidence of the x converter in a string
+ * hex_handler - Deals with the incidence of the x converter in a string.
  *
  * Description: Especific to ft_printf, it's a bad idea to
  * use it on other things.
  *
- * @args: A va_list
+ * @args: A va_list.
  *
  * Return: The number of chars printed.
  */
 int		hex_handler(char converter, va_list args);
 
 /**
- * percent_handler - Deals with the incidence of double % converters in a string
+ * percent_handler - Deals with the incidence of double % converters in a string.
  *
  * Description: Especific to ft_printf, it's a bad idea to
  * use it on other things.
  *
- * @args: A va_list
+ * @args: A va_list.
  *
  * Return: The number of chars printed.
  */
